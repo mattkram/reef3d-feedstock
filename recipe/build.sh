d730a89e48dc61f588911875efa13565ae501bc5
mattkram <mkramer@anaconda.com>
2023-10-27 07:23:05 -0500

    Refactor how we pass in make variables

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-HYPRE_DIR="${PREFIX}"
-
 # Supplement conda-forge environment variable with extra paths
-LDFLAGS="-L${HYPRE_DIR}/lib/ -lHYPRE $LDFLAGS"
+CXX="mpicxx"
+LDFLAGS="-lHYPRE $LDFLAGS"
+INCLUDE="-I${PREFIX}/include/eigen3 -DEIGEN_MPL2_ONLY"
 
-make -j "${CPU_COUNT}" CXX="mpicxx" HYPRE_DIR="${HYPRE_DIR}" EIGEN_DIR="${PREFIX}/include/eigen3" LDFLAGS="${LDFLAGS}"
+make -j "${CPU_COUNT}" CXX="${CXX}" INCLUDE="${INCLUDE}" LDFLAGS="${LDFLAGS}"
 
 mkdir -p "${PREFIX}/bin"
 cp bin/REEF3D "${PREFIX}/bin/reef3d"
