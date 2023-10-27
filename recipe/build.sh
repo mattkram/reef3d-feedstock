b5f1743d20956bcd238b1843dcb7b7f96dc65649
mattkram <mkramer@anaconda.com>
2023-10-27 07:19:45 -0500

    Change order of LDFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 HYPRE_DIR="${PREFIX}"
 
 # Supplement conda-forge environment variable with extra paths
-LDFLAGS="$LDFLAGS -L ${HYPRE_DIR}/lib/ -lHYPRE"
+LDFLAGS="-L${HYPRE_DIR}/lib/ -lHYPRE $LDFLAGS"
 
 make -j "${CPU_COUNT}" CXX="mpicxx" HYPRE_DIR="${HYPRE_DIR}" EIGEN_DIR="${PREFIX}/include/eigen3" LDFLAGS="${LDFLAGS}"
 
