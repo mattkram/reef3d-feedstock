25f6901f1eb3a83c37372c41c261bc797fd49a50
mattkram <mkramer@anaconda.com>
2023-10-27 10:17:09 -0500

    Revert "Try to pass in CXX as -cxx= to mpicxx"
    This reverts commit 9902a0e441e0f9fdf53fea6ed6b51d581996e684.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Supplement conda-forge environment variable with extra paths
-MPICXX="mpicxx -cxx=${CXX}"
+CXX="mpicxx"
 CXXFLAGS="$CXXFLAGS -w -std=c++11 -O3"
 LDFLAGS="-lHYPRE $LDFLAGS"
 INCLUDE="-I${PREFIX}/include/eigen3 -DEIGEN_MPL2_ONLY"
 
-make -j "${CPU_COUNT}" CXX="${MPICXX}" CXXFLAGS="${CXXFLAGS}" INCLUDE="${INCLUDE}" LDFLAGS="${LDFLAGS}"
+make -j "${CPU_COUNT}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" INCLUDE="${INCLUDE}" LDFLAGS="${LDFLAGS}"
 
 mkdir -p "${PREFIX}/bin"
 cp bin/REEF3D "${PREFIX}/bin/reef3d"
