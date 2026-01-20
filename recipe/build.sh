fe1707de26ed3916dbc0dac5741489d1c963d77b
mattkram <mkramer@anaconda.com>
2026-01-20 14:20:42 -0600

    fix: Pass in version and set branch variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,13 @@ if [[ $mpi == "openmpi" ]]; then
   export OPAL_PREFIX=${PREFIX}
 fi
 
+# These variables are required by the Makefile, and are passed in
+# from recipe
+GIT_VERSION="${VERSION}"
+GIT_BRANCH="master"
+
 CXX="mpicxx"
-CXXFLAGS="$CXXFLAGS -w -std=c++11 -O3"
+CXXFLAGS="$CXXFLAGS -w -std=c++11 -O3 -DVERSION=\"${GIT_VERSION}\" -DBRANCH=\"${GIT_BRANCH}\""
 LDFLAGS="-lHYPRE $LDFLAGS"
 INCLUDE="-I${PREFIX}/include/eigen3 -DEIGEN_MPL2_ONLY"
 
